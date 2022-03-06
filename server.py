85db938a8f50bf5f1f3e0f1528c7be2488c79562
chem <chem.chikweze@gmail.com>
2022-03-06 17:26:22 -0500

    make it better

--- server.py
+++ server.py
@@ -24,7 +24,7 @@ def webhook():
 @app.route('/github', methods = ['POST'])
 def api_gh_message():
     if request.headers['Content-Type']== 'application/json':
-        return json.dumps(request.json)
+        return "json.dumps(request.json)"
 
 if __name__ == '__main__':
     app.run(debug=True)
