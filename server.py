df7809fe05a73b6d7ad77b70a474793908eb6899
chem <chem.chikweze@gmail.com>
2022-03-06 17:18:08 -0500

    make it better

--- server.py
+++ server.py
@@ -6,20 +6,20 @@ app = Flask (__name__)
 def begin():
     return "hi"
 
-# @app.route('/webhook', methods=['POST'])
-# def webhook():
-#     if request.method == 'POST':
-#         print("hello webhook")
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print("hello webhook")
 
-#         webhook_msg = json.loads(request.data)
-#         ticker = webhook_msg['ticker']
-#         time = webhook_msg['time']
-#         volume = webhook_msg['bar']['volume']
+        webhook_msg = json.loads(request.data)
+        # ticker = webhook_msg['ticker']
+        # time = webhook_msg['time']
+        # volume = webhook_msg['bar']['volume']
         
-#         print(ticker, time, volume)
-#         return 'success', 200
-#     else:
-#         abort(400)
+        print(webhook_msg)
+        return 'success', 200
+    else:
+        abort(400)
 
 if __name__ == '__main__':
     app.run(debug=True)
